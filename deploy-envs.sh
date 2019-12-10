2fe9f5f01eb2ea68364d851c7ce3ef6d8daba0ea
Austin Mleziva <AustinMleziva@gmail.com>
2019-12-10 16:00:55 -0600

    Adding containerization, build, and travis configs

---
+++ deploy-envs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# set environment variables used in deploy.sh and AWS task-definition.json:
+export IMAGE_NAME=cloudpatterns
+export IMAGE_VERSION=latest
+
+export AWS_DEFAULT_REGION=us-east-2c
+export AWS_ECS_CLUSTER_NAME=default
+export AWS_VIRTUAL_HOST=cloudpatterns.austinmleziva.com
+
+# set any sensitive information in travis-ci encrypted project settings:
+# required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
