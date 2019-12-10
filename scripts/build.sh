3df82270ed4669566cb5ce3f92ab45967ddd0951
Austin Mleziva <AustinMleziva@gmail.com>
2019-12-10 16:35:15 -0600

    pointing build to correct folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ source ../deploy-envs.sh
 export AWS_ECS_REPO_DOMAIN=$AWS_ACCOUNT_NUMBER.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
 
 # Build process
-docker build -t $IMAGE_NAME ../
+docker build -t $IMAGE_NAME ../CloudPatterns
 docker tag $IMAGE_NAME $AWS_ECS_REPO_DOMAIN/$IMAGE_NAME:$IMAGE_VERSION
